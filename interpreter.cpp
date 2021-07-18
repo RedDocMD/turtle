a7bbfabc3eb28157b0703bba5917cb26ad88f926
Deep Majumder <deep.majumder2019@gmail.com>
2021-07-18 22:29:10 +0530

    Wrote proper parser

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,56 +1,96 @@
 #include "interpreter.h"
-#include <algorithm>
+
 #include <boost/algorithm/string.hpp>
+#include <boost/assert.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/log/trivial.hpp>
-#include <vector>
 
-std::optional<std::unique_ptr<Operation>>
-Interpreter::interpret(const std::string &command) {
+std::unique_ptr<Operation> Interpreter::interpret(const std::string &command) {
   auto comm = command;
   boost::trim(comm);
+  std::stringstream ss(comm);
 
+  return interpret_single(ss);
+}
+
+Interpreter::CommandType Interpreter::interpret_command_type(
+    std::stringstream &ss) {
+  CommandType comm_type;
+  std::string comm_name;
+  ss >> comm_name;
+  if (comm_name == "FD")
+    comm_type = CommandType::Fd;
+  else if (comm_name == "BK")
+    comm_type = CommandType::Bk;
+  else if (comm_name == "RT")
+    comm_type = CommandType::Rt;
+  else if (comm_name == "LT")
+    comm_type = CommandType::Lt;
+  else if (comm_name == "REPEAT")
+    comm_type = CommandType::Repeat;
+  else
+    comm_type = CommandType::Error;
+  return comm_type;
+}
+
+std::unique_ptr<Operation> Interpreter::interpret_single(
+    std::stringstream &ss) {
   try {
-    std::smatch match;
-    if (std::regex_search(comm, match, fd_reg)) {
-      int amt = boost::lexical_cast<int>(match[1]);
-      return std::make_unique<ForwardOperation>(amt);
-    } else if (std::regex_search(comm, match, bk_reg)) {
-      int amt = boost::lexical_cast<int>(match[1]);
-      return std::make_unique<BackwardOperation>(amt);
-    } else if (std::regex_search(comm, match, rt_reg)) {
-      int amt = boost::lexical_cast<int>(match[1]);
-      return std::make_unique<RightTurnOperation>(amt);
-    } else if (std::regex_search(comm, match, lt_reg)) {
-      int amt = boost::lexical_cast<int>(match[1]);
-      return std::make_unique<LeftTurnOperation>(amt);
-    } else if (std::regex_search(comm, match, rep_reg)) {
-      int cnt = boost::lexical_cast<int>(match[1]);
-      std::vector<std::string> part_comms;
-      std::string rep_comm = match[2];
-      // FIXME: This can't handle anything more than simple repeats
-      boost::split(part_comms, rep_comm, boost::is_any_of(","));
-      std::vector<std::optional<std::unique_ptr<Operation>>> comms(
-          part_comms.size());
-      std::transform(
-          part_comms.begin(), part_comms.end(),
-          comms.begin(), [&](const auto &str) -> auto {
-            return this->interpret(str);
-          });
-      if (std::all_of(
-              comms.begin(), comms.end(),
-              [](const auto &comm) -> auto { return comm.has_value(); })) {
-        std::vector<std::unique_ptr<Operation>> ops;
-        ops.reserve(comm.size());
-        for (auto &comm_opt : comms)
-          ops.push_back(std::move(*comm_opt));
-        return std::make_unique<RepeatOperation>(cnt, std::move(ops));
-      } else {
-        return {};
+    auto type = interpret_command_type(ss);
+    if (type == CommandType::Error) return {};
+
+    std::string tok;
+    ss >> tok;
+    switch (type) {
+      case CommandType::Fd: {
+        int amt = boost::lexical_cast<int>(tok);
+        return std::make_unique<ForwardOperation>(amt);
       }
+      case CommandType::Bk: {
+        int amt = boost::lexical_cast<int>(tok);
+        return std::make_unique<BackwardOperation>(amt);
+      }
+      case CommandType::Rt: {
+        int angle = boost::lexical_cast<int>(tok);
+        return std::make_unique<RightTurnOperation>(angle);
+      }
+      case CommandType::Lt: {
+        int angle = boost::lexical_cast<int>(tok);
+        return std::make_unique<LeftTurnOperation>(angle);
+      }
+      default:
+        BOOST_ASSERT_MSG(type != CommandType::Repeat,
+                         "Expected type to be repeat");
     }
-  } catch (const boost::bad_lexical_cast &) {
-    BOOST_LOG_TRIVIAL(debug) << "bad number";
+
+    // Handle repeat
+    int cnt = boost::lexical_cast<int>(tok);
+    return interpret_repeat(ss, cnt);
+  } catch (boost::bad_lexical_cast &err) {
+    BOOST_LOG_TRIVIAL(error) << "invalid number: " << err.what();
+    return {};
   }
-  return {};
+}
+
+std::unique_ptr<RepeatOperation> Interpreter::interpret_repeat(
+    std::stringstream &ss, int cnt) {
+  char c;
+  std::vector<std::unique_ptr<Operation>> ops;
+
+  ss >> std::skipws;
+  ss >> c;
+  if (c != '(') return {};
+  while (true) {
+    auto op = interpret_single(ss);
+    if (!op) return {};
+    ops.push_back(std::move(op));
+    ss >> std::skipws;
+    ss >> c;
+    if (c == ')')
+      return std::make_unique<RepeatOperation>(cnt, std::move(ops));
+    else if (c == ',')
+      ss >> std::skipws;
+    else
+      return {};
+  }
 }
--- interpreter.h
+++ interpreter.h
@@ -1,22 +1,29 @@
 #ifndef __INTERPRETER_H__
 #define __INTERPRETER_H__
 
-#include "turtle.h"
 #include <memory>
-#include <optional>
-#include <regex>
+#include <sstream>
 #include <string>
 
-class Interpreter {
-  std::regex fd_reg{R"(^FD (\d+)$)"};
-  std::regex bk_reg{R"(^BK (\d+)$)"};
-  std::regex rt_reg{R"(^RT (\d+)$)"};
-  std::regex lt_reg{R"(^LT (\d+)$)"};
-  std::regex rep_reg{R"(^REPEAT (\d+) \(([\s\w\d,]+)\)$)"};
-  std::regex comma_sep{R"(,\s*)"};
+#include "turtle.h"
 
-public:
-  std::optional<std::unique_ptr<Operation>> interpret(const std::string &command);
+class Interpreter {
+  enum class CommandType {
+    Fd,
+    Bk,
+    Rt,
+    Lt,
+    Repeat,
+    Error,
+  };
+
+  static CommandType interpret_command_type(std::stringstream &ss);
+  static std::unique_ptr<Operation> interpret_single(std::stringstream &ss);
+  static std::unique_ptr<RepeatOperation> interpret_repeat(
+      std::stringstream &ss, int cnt);
+
+ public:
+  static std::unique_ptr<Operation> interpret(const std::string &command);
 };
 
-#endif // __INTERPRETER_H__
+#endif  // __INTERPRETER_H__
