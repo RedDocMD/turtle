593e7a7223714c76c7654782a9fabbef74d6343b
Deep Majumder <deep.majumder2019@gmail.com>
2021-07-19 14:58:03 +0530

    Added GPLv2 license

--- geom.h
+++ geom.h
@@ -1,3 +1,22 @@
+/*
+ *   Turtle - A basic implementation of LOGO
+ *   Copyright (C) 2021 Deep Majumder
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #ifndef __GEOM_H__
 #define __GEOM_H__
 
@@ -6,8 +25,8 @@
 struct Point {
   double x, y;
   Point(double x, double y) : x{x}, y{y} {}
-  bool operator==(const Point &ot) { return x == ot.x && y == ot.y; }
-  bool operator!=(const Point &ot) { return x != ot.x || y != ot.y; }
+  bool operator==(const Point &ot) const { return x == ot.x && y == ot.y; }
+  bool operator!=(const Point &ot) const { return x != ot.x || y != ot.y; }
 };
 
 std::ostream &operator<<(std::ostream &os, const Point &pt);
