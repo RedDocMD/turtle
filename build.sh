5db36c3f705e711f2778accb6608132b8c7208c5
Deep Majumder <deep.majumder2019@gmail.com>
2021-07-23 22:37:00 +0530

    Update build directory

--- build.sh
+++ build.sh
@@ -10,26 +10,25 @@ NAME=Turtle
 REPO_DIR=${PWD}
 
 # Build
-mkdir build
+[ -e build ] || mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
 
 # Create .desktop file
 DESKTOP_FILE=turtle.desktop
+echo "Creating desktop file ..."
 echo "[Desktop Entry]" > $DESKTOP_FILE
 {
   echo "Version=${VERSION}"
   echo "Type=Application"
   echo "Name=${NAME}"
   echo "Comment=A simple LOGO implementation"
-  echo "TryExec=${PWD}/turtle"
-  echo "Exec=${PWD}/turtle"
+  echo "TryExec=\"GTK_THEME=Adwaita:light ${PWD}/turtle\""
+  echo "Exec=\"GTK_THEME=Adwaita:light ${PWD}/turtle\""
   echo "Icon=${REPO_DIR}/assets/logo.svg"
 } >> $DESKTOP_FILE
 
 # Copy .desktop file
-if [[ ! -e ~/.local/share/applications ]]; then
-  mkdir ~/.local/share/applications
-fi
-cp $DESKTOP_FILE ~/.local/share/applications
+echo "Copying desktop file to /usr/share/applications"
+sudo cp $DESKTOP_FILE /usr/share/applications
